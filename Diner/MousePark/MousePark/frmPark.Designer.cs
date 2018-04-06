87c675306477a92a3e8f2ca9375099983d3ea605
Haishi Bai <hbai@microsoft.com>
2018-04-06 13:28:32 -0700

    mouse park

---
+++ Diner/MousePark/MousePark/frmPark.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MousePark
+{
+    partial class frmPark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmPark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(227, 96);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmPark";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Mouse Park";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmPark_Load);
+            this.Click += new System.EventHandler(this.frmPark_Click);
+            this.MouseEnter += new System.EventHandler(this.frmPark_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.frmPark_MouseLeave);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Diner/MousePark/MousePark/frmPark.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MousePark
+{
+    public partial class frmPark : Form
+    {
+        private bool isParked = false;
+        public frmPark()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPark_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void frmPark_MouseLeave(object sender, EventArgs e)
+        {
+            if (isParked)
+                Application.Exit();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{F15}");
+        }
+
+        private void frmPark_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+        }
+
+        private void frmPark_Click(object sender, EventArgs e)
+        {
+            isParked = true;
+            this.BackColor = Color.Yellow;
+            if (!timer1.Enabled)
+            {
+                timer1.Interval = 60000;
+                timer1.Enabled = true;
+            }
+        }
+    }
+}
